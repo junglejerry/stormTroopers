fbc8c55a67dfc87f7d869ca612cf1943150a6c93
Jerry John Comellas <jerry.comellas@gmail.com>
2015-05-15 10:46:03 -0400

    update to scripts page - moving from hard coding names to parsing from swAPI

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
     new WOW().init();
 
 
-    //Simple Text Rotator
-    $(".rotate").textrotator({
-        animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
-        separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-        speed: 9000 // How many milliseconds until the next word show.
-    });
+    // //Simple Text Rotator
+    // $(".rotate").textrotator({
+    //     animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+    //     separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+    //     speed: 9000 // How many milliseconds until the next word show.
+    // });
 
 });
 
@@ -20,31 +20,51 @@ var numMismatches = 0;
 var score = 0;
 var count = 0;
 var people = [];
-var characterArray = ["Luke Skywalker", "Luke Skywalker", "C-3PO", "C-3PO", "R2-D2", "R2-D2", "Darth Vader", "Darth Vader", "Leia Organa", "Leia Organa", "Owen Lars", "Owen Lars", "Beru Whitesun lars", "Beru Whitesun lars", "R5-D4", "R5-D4", "Biggs Darklighter", "Biggs Darklighter", "Obi-Wan Kenobi", "Obi-Wan Kenobi", "Anakin Skywalker", "Anakin Skywalker", "Wilhuff Tarkin", "Wilhuff Tarkin", "Chewbacca", "Chewbacca", "Han Solo", "Han Solo", "Greedo", "Greedo"];
+//var characterArray = [];  <- This array replaces the fullDeck array if you want to hard code the card names.
+//["Luke Skywalker", "Luke Skywalker", "C-3PO", "C-3PO", "R2-D2", "R2-D2", "Darth Vader", "Darth Vader", "Leia Organa", "Leia Organa", "Owen Lars", "Owen Lars", "Beru Whitesun lars", "Beru Whitesun lars", "R5-D4", "R5-D4", "Biggs Darklighter", "Biggs Darklighter", "Obi-Wan Kenobi", "Obi-Wan Kenobi", "Anakin Skywalker", "Anakin Skywalker", "Wilhuff Tarkin", "Wilhuff Tarkin", "Chewbacca", "Chewbacca", "Han Solo", "Han Solo", "Greedo", "Greedo"];
 var memoryValues = [];
 var memoryCards = [];
 var cardsFlipped = 0;
+var fullDeck = [];
 
-    // Create an XMLHTTP Request object.
-        for (i = 1; i < 3; i++) {
-            var req = new XMLHttpRequest();
+// Create an XMLHTTP Request object.
+for (i = 1; i < 3; i++) {
+    var req = new XMLHttpRequest();
 
-        //  Create a callback function.
-            req.onload = function() {
+    //  Create a callback function.
+    req.onload = function() {
 
-                people = people.concat(JSON.parse(this.responseText).results);
-                count++;
-                if (count == 2) {
-                    newBoard(people);
-                }
-            }
-        // Open a request
-            req.open("GET", "http://swapi.co/api/people/?page=" + i);
+        people = people.concat(JSON.parse(this.responseText).results);
+        count++;
 
-        //  Send the Request
-            req.send();
+        if (count == 2) {
+
+            buildDeck(people);
         }
-        console.log(req);
+    }
+
+    // Open a request
+    req.open("GET", "http://swapi.co/api/people/?page=" + i);
+
+    //  Send the Request
+    req.send();
+}
+
+// Building the deck
+
+function buildDeck(people) {
+    // Loop will run 15 times to gather 15 characters
+    for (i = 0; i < 15; i++) {
+        // double the list of same names
+        for (x = 0; x < numMatches; x++) {
+                fullDeck.push(people[i].name);
+            }
+    }
+        console.log(fullDeck);
+
+    newBoard(people);
+}
+
 
 
 // method for shuffling cards
@@ -60,16 +80,16 @@ Array.prototype.memoryCardsShuffle = function() {
     }
 }
 
-// method for building the deck - after cards shuffle, start a new game
+// after cards shuffle, start a new game
 
 function newBoard(people) {
     console.log("starting a new game");
     cardsFlipped = 0;
     var output = "";
-    characterArray.memoryCardsShuffle();
+    fullDeck.memoryCardsShuffle();
 
-    for (var i = 0; i < characterArray.length; i++) {
-        output += '<div id="card_' + i + '" onclick="memoryFlipCard(this,\'' + characterArray[i] + '\')"></div>';
+    for (var i = 0; i < fullDeck.length; i++) {
+        output += '<div id="card_' + i + '" onclick="memoryFlipCard(this,\'' + fullDeck[i] + '\')"></div>';
     }
     console.log("output content: " + output);
     document.getElementById("memoryBoard").innerHTML = output;
@@ -89,11 +109,14 @@ function memoryFlipCard(card, val) {
             memoryCards.push(card.id);
             if (memoryValues[0] == memoryValues[1]) {
                 cardsFlipped += 2;
+
+                //At this point a successful match has occurred
+
                 // Clear both arrays
                 memoryValues = [];
                 memoryCards = [];
                 // Check to see if the whole board is cleared
-                if (cardsFlipped == characterArray.length) {
+                if (cardsFlipped == fullDeck.length) {
                     alert("The force is strong with this one. Want to play again?");
                     document.getElementById('memoryBoard').innerHTML = "";
                     newBoard();
@@ -125,4 +148,3 @@ function memoryFlipCard(card, val) {
 
 //Occasionally, the same origin policy may block requests between subdomains on the same domain. This code is here to prevent that problem here in JavaScript.
 //document.domain = 'localhost';
-
